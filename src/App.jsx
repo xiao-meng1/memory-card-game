96f4f78ffc2e2b991a8ed0cd0c793ba47a07f23c
Xiao <xmeng1@ualberta.ca>
2022-06-10 17:03:24 -0600

    add stateand click event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './components/Card';
 import styles from './styles/app.module.css';
 import marioImg from './images/500px-Mario_SSBU.webp';
@@ -14,79 +14,117 @@ import nessImg from './images/500px-Ness_SSBU.webp';
 import captainFalconImg from './images/500px-Captain_Falcon_SSBU.webp';
 import jigglypuffImg from './images/500px-Jigglypuff_SSBU.webp';
 
-function App() {
-  const cardItems = [
-    {
-      name: 'Mario',
-      URL: marioImg,
-    },
-    {
-      name: 'Donkey Kong',
-      URL: donkeyKongImg,
-    },
-    {
-      name: 'Link',
-      URL: linkImg,
-    },
-    {
-      name: 'Samus',
-      URL: samusImg,
-    },
-    {
-      name: 'Yoshi',
-      URL: yoshiImg,
-    },
-    {
-      name: 'Kirby',
-      URL: kirbyImg,
-    },
-    {
-      name: 'Fox',
-      URL: foxImg,
-    },
-    {
-      name: 'Pikachu',
-      URL: pikachuImg,
-    },
-    {
-      name: 'Luigi',
-      URL: luigiImg,
-    },
-    {
-      name: 'Ness',
-      URL: nessImg,
-    },
-    {
-      name: 'Captain Falcon',
-      URL: captainFalconImg,
-    },
-    {
-      name: 'Jigglypuff',
-      URL: jigglypuffImg,
-    },
-  ];
+const cardItems = [
+  {
+    name: 'Mario',
+    URL: marioImg,
+  },
+  {
+    name: 'Donkey Kong',
+    URL: donkeyKongImg,
+  },
+  {
+    name: 'Link',
+    URL: linkImg,
+  },
+  {
+    name: 'Samus',
+    URL: samusImg,
+  },
+  {
+    name: 'Yoshi',
+    URL: yoshiImg,
+  },
+  {
+    name: 'Kirby',
+    URL: kirbyImg,
+  },
+  {
+    name: 'Fox',
+    URL: foxImg,
+  },
+  {
+    name: 'Pikachu',
+    URL: pikachuImg,
+  },
+  {
+    name: 'Luigi',
+    URL: luigiImg,
+  },
+  {
+    name: 'Ness',
+    URL: nessImg,
+  },
+  {
+    name: 'Captain Falcon',
+    URL: captainFalconImg,
+  },
+  {
+    name: 'Jigglypuff',
+    URL: jigglypuffImg,
+  },
+];
 
-  const randomOrder = [1, 2, 3, 4, 0, 11, 10, 9, 8, 7, 6, 5];
+const generateRandomOrder = () => {
+  const remainingNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  const randomOrder = [];
+
+  for (let i = 0; i < 12; i += 1) {
+    const randomIndex = Math.floor(Math.random() * remainingNumbers.length);
+    randomOrder.push(remainingNumbers.splice(randomIndex, 1));
+  }
+
+  return randomOrder;
+};
+
+function App() {
+  const [highScore, setHighScore] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [clickedNames, setClickedNames] = useState([]);
+  const [cardsOrder, setCardsOrder] = useState(generateRandomOrder());
+
+  const onCardClick = (e) => {
+    const clickedName = e.currentTarget.dataset.name;
+
+    setCardsOrder(generateRandomOrder());
+
+    if (clickedNames.includes(clickedName)) {
+      if (currentScore > highScore) {
+        setHighScore(currentScore);
+      }
+
+      setCurrentScore(0);
+      setClickedNames([]);
+
+      return;
+    }
+
+    setCurrentScore(currentScore + 1);
+    setClickedNames([...clickedNames, clickedName]);
+  };
 
   return (
     <div className={styles.app}>
       <header className={styles.header}>
         <div className={styles.left}>
           <h1>Memory Card Game</h1>
-          <p>Instructions: Click on cards but don&apos;t repeat yourself!</p>
+          <p className={styles.p}>
+            Instructions: Click on cards but don&apos;t repeat yourself!
+          </p>
         </div>
         <div className={styles.right}>
-          <p>High Score: x</p>
-          <p>Current Score: y</p>
+          <p className={styles.p}>High Score: {highScore}</p>
+          <p className={styles.p}>Current Score: {currentScore}</p>
         </div>
       </header>
       <main className={styles.main}>
         <div className={styles.container}>
-          {randomOrder.map((indexItem) => (
+          {cardsOrder.map((indexItem) => (
             <Card
               key={cardItems[indexItem].name}
               name={cardItems[indexItem].name}
               imgURL={cardItems[indexItem].URL}
+              clickEvent={onCardClick}
             />
           ))}
         </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,24 +3,38 @@ import PropTypes from 'prop-types';
 import styles from '../styles/card.module.css';
 
 function Card(props) {
-  const { imgURL, name } = props;
+  const { imgURL, name, clickEvent } = props;
 
   return (
-    <div className={styles.card}>
-      <img className={styles.img} src={imgURL} alt="character" />
-      <h2 className={styles.h2}>{name}</h2>
-    </div>
+    <button
+      type="button"
+      data-name={name}
+      className={styles.card}
+      onClick={clickEvent}
+    >
+      <img
+        className={styles.img}
+        draggable="false"
+        src={imgURL}
+        alt="character"
+      />
+      <h2 className={styles.h2} draggable="false">
+        {name}
+      </h2>
+    </button>
   );
 }
 
 Card.defaultProps = {
   imgURL: '',
   name: '',
+  clickEvent: () => {},
 };
 
 Card.propTypes = {
   imgURL: PropTypes.string,
   name: PropTypes.string,
+  clickEvent: PropTypes.func,
 };
 
 export default Card;
